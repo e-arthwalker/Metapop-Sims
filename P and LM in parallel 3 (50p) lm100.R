4397fe741516d1b35e076f63ff48b46ae016dfc4
e-arthwalker <38920686+e-arthwalker@users.noreply.github.com>
2018-05-02 15:56:18 -0400

    Update P and LM in parallel 3 (50p) lm100.R

--- P and LM in parallel 3 (50p) lm100.R
+++ P and LM in parallel 3 (50p) lm100.R
@@ -16,14 +16,6 @@ source("Vary Alpha Destroy and Degrade Function for parallel.r") #provides repli
 #across a landscape type
 #############################################################################################################
 
-#SET WHERE YOU WANT THE OUTPUT TO GO ***modify this appropiately***
-#############################################################################################################
-#############################################################################################################
-setwd("C:/Users/Administrator/Desktop/Final Metapop R Package/changing to mean min/50 patches")
-#access working directory that all my metapop functions are stored in
-#############################################################################################################
-#############################################################################################################
-
 #SET UP TO PROCESS IN PARALLEL (The simulations take a long time otherwise)
 #############################################################################################################
 library("doParallel") 
@@ -34,24 +26,35 @@ registerDoParallel(cl)
 #specify the number of simulation iterations wanted ***modify this appropiately***
 ##############################################################################################################
 ##############################################################################################################
+setwd("C:/Users/Administrator/Desktop/Final Metapop R Package/changing to mean min/50 patches")
+#specify where you want the output
 its.of.5=100 #enter how many iterations total you want divided by 5
+n.patches=50
+landscape.limit=100
+no.runs=500 #enter desired degree of clustering/declustering here (higher number = higher clustering/regularity
+#respectively)
+scaled.lm=100 #enter desired pristine persistence capacity
+file.name="500reps_PnLM_3_50p_lm100.csv" #enter desired output file name
 ##############################################################################################################
 ##############################################################################################################
 
-#PROCESS TO BE RUN ON EACH CORE: NOTE! for each alpha value 5 sepperate cores run a specified number of iterations to give 5*that number 
-#of iterations for each alpha value for each alpha value 5 sepperate cores run 100 iterations to give 500 
-#iterations for each alpha value
+#PROCESS TO BE RUN ON EACH CORE: NOTE! for each alpha value 5 sepperate cores run a specified number of 
+#iterations to give 5*that number of iterations for each alpha value for each alpha value 5 sepperate cores 
+#run 100 iterations to give 500 iterations for each alpha value
 ############################################################################################################
-parallel.alphas<-function(j, n.reps, k){
+parallel.alphas<-function(j, n.reps, k, n.patches, landscape.limit, no.runs, scaled.lm){
   alpha.data<-read.csv("C:/Users/Administrator/Desktop/Final Metapop R Package/changing to mean min/50 patches/alphas.csv")
   #alpha.data<-alpha.data[,-1]
   alpha.data<-alpha.data[,-10]
   regular.data<-vary.alpha.in.parallel(alphas=alpha.data[1,j+1], landscape.type="regular", n.landscapes=n.reps,
-                                       k=k, n.patches=50, landscape.limit=100, no.runs=500, scaled.lm=100)
+                                       k=k, n.patches=n.patches, landscape.limit=landscape.limit, no.runs=no.runs, 
+                                       scaled.lm=scaled.lm)
   random.data<-vary.alpha.in.parallel(alphas=alpha.data[2,j+1], landscape.type="random", n.landscapes=n.reps,
-                                      k=k, n.patches=50, landscape.limit=100, no.runs=500, scaled.lm=100)
+                                      k=k, n.patches=n.patches, landscape.limit=landscape.limit, no.runs=no.runs, 
+                                      scaled.lm=scaled.lm)
   clustered.data<-vary.alpha.in.parallel(alphas=alpha.data[3,j+1], landscape.type="clustered", n.landscapes=n.reps,
-                                         k=k, n.patches=50, landscape.limit=100, no.runs=500, scaled.lm=100)
+                                         k=k, n.patches=n.patches, landscape.limit=landscape.limit, no.runs=no.runs, 
+                                         scaled.lm=scaled.lm)
   rbind(regular.data, random.data, clustered.data)
 }
 ##############################################################################################################
@@ -59,8 +62,11 @@ parallel.alphas<-function(j, n.reps, k){
 #GET THE DATA
 ##############################################################################################################
 output<- foreach (k=1:5, .combine=rbind)  %:%
- foreach(i=((k*8)-7):(k*8), .combine=rbind) %dopar% parallel.alphas(j=i-((k-1)*8), n.reps=its.of.5, k=k)
-write.csv(output, "500reps_PnLM_3_50p_lm100.csv")
+ foreach(i=((k*8)-7):(k*8), .combine=rbind) %dopar% parallel.alphas(j=i-((k-1)*8), n.reps=its.of.5, k=k,
+                                                                    n.patches=n.patches, 
+                                                                    landscape.limit=landscape.limit, 
+                                                                    no.runs=no.runs, scaled.lm=scaled.lm)
+write.csv(output, file.name)
 #############################################################################################################
 
 # turn parallel processing off and run sequentially again:
