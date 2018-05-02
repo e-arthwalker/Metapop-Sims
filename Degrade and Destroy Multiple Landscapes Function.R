2218ff2e83624aabcc9e7f97d81548f8026f22aa
e-arthwalker <38920686+e-arthwalker@users.noreply.github.com>
2018-05-02 14:32:02 -0400

    Update Degrade and Destroy Multiple Landscapes Function.R

--- Degrade and Destroy Multiple Landscapes Function.R
+++ Degrade and Destroy Multiple Landscapes Function.R
@@ -9,17 +9,23 @@
 #scaled.lm = value you would like to scale lambda.M to initially
 ############################################################################################
 gen.destroy.and.degrade.landscapes<-function(n.patches, landscape.limit, landscape.type, no.runs, a, n.landscapes, scaled.lm){
-  for (i in 1:n.landscapes){
-    landscape<-create.landscape(n.patches=n.patches, landscape.limit=landscape.limit, landscape.type=landscape.type, no.runs=no.runs)
+  for (i in 1:n.landscapes){ #for each landscape
+    landscape<-create.landscape(n.patches=n.patches, landscape.limit=landscape.limit, landscape.type=landscape.type, no.runs=no.runs) 
+    #create that landscape
     #scaling delta starting with initial lambda.M of 20
-    delta<-(lambda.M=lambda.M.function(landscape=landscape, a=a, delta=1))/scaled.lm
-    output<-destroy.vs.degrade(landscape=landscape, a=a, delta=delta)
-    output<-data.frame(landscape.type, output)
+    delta<-(lambda.M=lambda.M.function(landscape=landscape, a=a, delta=1))/scaled.lm #scale delta to achieve the desired initial 
+    #persistence capacity of the pristine landscape (this is done to make the impacts of degradation and destruction comparable 
+    #across landscapes
+    output<-destroy.vs.degrade(landscape=landscape, a=a, delta=delta) #sequentially destroy patches and degrade copies of the landscape
+    #until no habitat remains, while calculating metrics measuring their impact until no habitat remains
+    output<-data.frame(landscape.type, output) #include the respective landscape type with the output
     #shift % loss column, (fix this in the destroy vs degrade function then remove this here)
-    output["percent.habitatloss"]<-c(0, head(output["percent.habitatloss"], dim(output)[1]-1)[[1]])
-    output$landscape.type<-landscape.type
-    output$alpha<-a
-    output$rep.no<-i
+    output["percent.habitatloss"]<-c(0, head(output["percent.habitatloss"], dim(output)[1]-1)[[1]]) #Adjust the percent habitat loss
+    #column so that it starts from zero
+    output$landscape.type<-landscape.type #the output column landscape.type should contain the landscape type
+    output$alpha<-a #the output column alpha should contain alpha's value
+    output$rep.no<-i #provide a rep no to identify to which landscape the data belongs
+    #combine the data
     if(i==1){data<-output}
     else{data<-rbind(data, output)}
   }
